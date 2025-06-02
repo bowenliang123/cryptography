b2b6cf2b9df3e7c087ac4f33bf8d69c2623638b8
Bowen Liang <liangbowen@gf.com.cn>
2025-06-02 23:56:58 +0800

    introduce sha256sum tool

--- provider/crypto.py
+++ provider/crypto.py
@@ -3,6 +3,8 @@ from typing import Any
 from dify_plugin import ToolProvider
 from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
+from tools.sha256sum.sha256sum_tool import Sha256SumTool
+
 
 class CryptoProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
@@ -10,5 +12,6 @@ class CryptoProvider(ToolProvider):
             """
             IMPLEMENT YOUR VALIDATION HERE
             """
+            Sha256SumTool.from_credentials({})
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
---
+++ tools/sha256sum/sha256sum_tool.py
@@ -0,0 +1,19 @@
+from collections.abc import Generator
+from typing import Any
+
+from cryptography.hazmat.primitives import hashes
+from dify_plugin import Tool
+from dify_plugin.entities.tool import ToolInvokeMessage
+from dify_plugin.file.file import File
+
+
+class Sha256SumTool(Tool):
+    def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
+        input_file: File = tool_parameters.get("input_file")
+        if not input_file or not isinstance(input_file, File):
+            raise ValueError("Not a valid file for input input_file")
+
+        digest = hashes.Hash(hashes.SHA256())
+        digest.update(input_file.blob)
+        hash_result = digest.finalize().hex()
+        yield self.create_text_message(hash_result)
